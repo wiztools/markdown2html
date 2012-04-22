7c0cb2b89215b8e030fccd5e95ede6f56f306077
Subhash Chandran <subwiz@gmail.com>
2012-04-22 16:32:05 +0000

    initial import markdown2html

---
+++ src/main/java/org/wiztools/markdown2html/Markdown2HtmlMain.java
@@ -0,0 +1,26 @@
+package org.wiztools.markdown2html;
+
+import java.io.*;
+import org.tautua.markdownpapers.Markdown;
+import org.tautua.markdownpapers.parser.ParseException;
+
+/**
+ *
+ * @author subwiz
+ */
+public class Markdown2HtmlMain {
+    private static void printHelp(PrintStream out) {
+        out.println("Note: Input is read from STDIN and output written to STDOUT.");
+        out.println();
+    }
+    
+    public static void main(String[] arg) throws ParseException, IOException {
+        printHelp(System.err);
+        Reader in = new InputStreamReader(System.in);
+        Writer out = new OutputStreamWriter(System.out);
+        Markdown md = new Markdown();
+        md.transform(in, out);
+        out.flush();
+        out.close();
+    }
+}
